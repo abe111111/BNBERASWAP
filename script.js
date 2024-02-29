601d7cf996400f7befd7b611bd89715a7a806dce
abe111111 <73217549+abe111111@users.noreply.github.com>
2024-02-28 23:08:46 -0330

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,115 @@
-// JavaScript code for Web3 and contract interaction
 // Initialize Web3
 const web3 = new Web3(Web3.givenProvider || "ws://localhost:8545");
 
 // Contract ABI
 const abi = [
-    // Paste your contract ABI here
+    {
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "amount",
+                "type": "uint256"
+            }
+        ],
+        "name": "bridgeToBeraChain",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "amount",
+                "type": "uint256"
+            }
+        ],
+        "name": "bridgeToBSC",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "stateMutability": "nonpayable",
+        "type": "constructor"
+    },
+    {
+        "anonymous": false,
+        "inputs": [
+            {
+                "indexed": true,
+                "internalType": "address",
+                "name": "sender",
+                "type": "address"
+            },
+            {
+                "indexed": false,
+                "internalType": "uint256",
+                "name": "amount",
+                "type": "uint256"
+            },
+            {
+                "indexed": false,
+                "internalType": "string",
+                "name": "fromChain",
+                "type": "string"
+            },
+            {
+                "indexed": false,
+                "internalType": "string",
+                "name": "toChain",
+                "type": "string"
+            }
+        ],
+        "name": "TokensBridged",
+        "type": "event"
+    },
+    {
+        "inputs": [],
+        "name": "berachainUSDCAddress",
+        "outputs": [
+            {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "bscUSDTAddress",
+        "outputs": [
+            {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "owner",
+        "outputs": [
+            {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    }
 ];
 
 // Contract addresses
 const contractAddresses = {
-    berachainUSDC: "0xYourBerachainUSDCAddress",
-    bscUSDT: "0xYourBSCUSDTAddress"
+    berachainUSDC: "0x959723c105eD00cc51Deb67FFdeDdD42c6390B2C",
+    bscUSDT: "0x5E10a91adb891831378A6A035f9B05382B45Aa15"
 };
 
 // Initialize contract instance
